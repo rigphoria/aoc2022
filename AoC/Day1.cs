2f5854ad9b3bdf04b21a4e3b7e3a99dbf18663cb
ryan <rigphoria@gmail.com>
2022-12-04 13:51:48 -0500

    day 1

---
+++ AoC/Day1.cs
@@ -0,0 +1,24 @@
+﻿namespace AoC;
+using System.IO;
+
+public class Day1{
+	public void Solve(){
+		int calorieCounter = 0;
+		int highestCalories = 0;
+		string[] input_lines = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "day1_input.txt"));
+
+		for (int i = 0; i < input_lines.Length; i++) {
+			if(String.IsNullOrWhiteSpace(input_lines[i])){
+				if (calorieCounter > highestCalories) {
+					highestCalories = calorieCounter;
+					calorieCounter = 0;
+					continue;
+				}
+				calorieCounter = 0;
+				continue;
+			}
+			calorieCounter += Int32.Parse(input_lines[i]);
+		}
+		Console.Write("day 1: " + highestCalories);
+	}
+}
---
+++ AoC/Main.cs
@@ -0,0 +1,8 @@
+﻿namespace AoC;
+
+public class App{
+	static void Main(string[] args){
+		Day1 day1 = new Day1();
+		day1.Solve();
+	}
+}
